11d0a9494cb82538d1b48f3003d40e9af50b413e
Matheus <matheussogon17@gmail.com>
2025-02-21 18:00:40 -0300

    Criando as classes e atributos

--- sistema_hotel_f-luxo.js
+++ sistema_hotel_f-luxo.js
@@ -1,6 +1,45 @@
 const requisicao = require('readline-sync'); //comando necessario para simular um input em js
 
-// Captura a entrada do usuário
-//let nome = requisicao.question('Digite seu nome: ');
+class Reserva {
+    constructor(id, cliente_id, status, check_in, check_out) {
+        this.id = id;
+        this.cliente_id = cliente_id;
+        this.status = status;
+        this.check_in = check_in;
+        this.check_out = check_out;
+    }
+}
 
-//console.log(`Olá, ${nome}!`);
+class Funcionario {
+    constructor(id, nome_usuario, cpf, email, senha) {
+        this.id = id;
+        this.nome_usuario = nome_usuario;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+}
+
+class Cliente {
+    constructor(id, nome, data_nascimento, cpf, email, senha) {
+        this.id = id;
+        this.nome = nome;
+        this.data_nascimento = data_nascimento;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+}
+
+class Quartos {
+    constructor(quantidade_camas, preco_noite, nome, descricao) {
+        this.quantidade_camas = quantidade_camas;
+        this.preco_noite = preco_noite;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+}
+
+class Sistema {
+    constructor()
+}
