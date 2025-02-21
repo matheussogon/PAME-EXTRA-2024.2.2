69c52a0cd4fba1fe8ff7835915728761b53a511e
Matheus <matheussogon17@gmail.com>
2025-02-21 16:52:52 -0300

    Gerando o arquivo do codigo do sistema e instalando o readline-sync

---
+++ sistema_hotel_f-luxo.js
@@ -0,0 +1,6 @@
+const requisicao = require('readline-sync'); //comando necessario para simular um input em js
+
+// Captura a entrada do usuário
+//let nome = requisicao.question('Digite seu nome: ');
+
+//console.log(`Olá, ${nome}!`);
